86917437716f6ed031005316b49127d78ac70af2
ancm2@kent.ac.uk <ancm2@kent.ac.uk>
2016-09-26 17:03:31 +0200

    Media player

---
+++ MyWindowsMediaPlayer/View/ScreenView.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System.Drawing;
+using Microsoft.Win32;
+using System;
+using System.Windows.Media;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Threading;
+
+namespace MyWindowsMediaPlayer.View
+{
+    /// <summary>
+    /// Logique d'interaction pour ScreenView.xaml
+    /// </summary>
+    public partial class ScreenView : UserControl
+    {
+        private String mediaPath = "";
+        private String[] files;
+        private int mediaTime;
+        private int mediaActualPos;
+        private int mediaActualFile;
+
+        public ScreenView()
+        {
+            InitializeComponent();
+            mediaActualFile = 0;
+        }
+
+        internal void Source(String path)
+        {
+            if (path != null)
+            {
+                mediaPath = path;
+                media.Source = new Uri(path, UriKind.RelativeOrAbsolute);
+                //files[mediaActualFile],
+            }
+        }
+
+        internal void Play()
+        {
+            if (media.Source != null)
+                media.Play();
+        }
+
+
+        internal void Pause()
+        {
+            if (media.Source != null)
+                media.Pause();
+        }
+
+        internal void Stop()
+        {
+            if (media.Source != null)
+            {
+                mediaTime = 0;
+                timeSlider.Value = 0;
+                media.Source = null;
+                media.Stop();
+            }
+        }
+
+        private void Element_Mediaopened(object sender, RoutedEventArgs e)
+        {
+            Play();
+            timeSlider.Maximum = media.NaturalDuration.TimeSpan.TotalMilliseconds;
+            mediaTime = (int)media.NaturalDuration.TimeSpan.TotalMilliseconds;
+            mediaActualPos = (int)media.Position.TotalMilliseconds;
+        }
+
+        private void Element_Mediaended(object sender, RoutedEventArgs e)
+        {
+
+            Stop();
+            if (files != null && files.Length > mediaActualFile + 1)
+            {
+                mediaActualFile++;
+                media.Source = new Uri(files[mediaActualFile]);
+                Play();
+            }
+        }
+
+        private void MediaTimeChanged(object sender, RoutedEventArgs e)
+        {
+            TimeSpan time = new TimeSpan(0, 0, 0, 0, (int)timeSlider.Value);
+            media.Position = time;
+            mediaActualPos = (int)media.Position.TotalMilliseconds;
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += worker_DoWork;
+            worker.ProgressChanged += worker_ProgressChanged;
+            worker.RunWorkerAsync();
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int i;
+
+            while (true)
+            {
+                if (this.media != null && mediaTime != 0)
+                {
+                    i = mediaActualPos / mediaTime * 100;
+                    if (i <= 0)
+                        i = 1;
+                    while (i < 100 && mediaTime > 0)
+                    {
+                        if (i == 1)
+                            (sender as BackgroundWorker).ReportProgress(i);
+                        else
+                            (sender as BackgroundWorker).ReportProgress(i);
+                        Thread.Sleep((this.mediaTime) / 100);
+                        i++;
+                    }
+                    if (mediaTime < 0)
+                        (sender as BackgroundWorker).ReportProgress(0);
+                    this.mediaTime = 0;
+                }
+            }
+
+
+        }
+
+        void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Progress.Value = e.ProgressPercentage;
+        }
+    }
+}
---
+++ MyWindowsMediaPlayer/ViewModel/ControlViewModel.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Win32;
+using MVVMDemoApplication.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MyWindowsMediaPlayer.View;
+
+namespace MyWindowsMediaPlayer.ViewModel
+{
+    class ControlViewModel : ViewModelBase
+    {
+        private ScreenView media;
+
+        #region fields
+        private ICommand openCommand;
+        private ICommand playCommand;
+        private ICommand pauseCommand;
+        private ICommand stopCommand;
+        #endregion
+        
+        public ControlViewModel(ScreenView view)
+        {
+            this.media = view;
+        }
+
+        #region properties
+        public ICommand OpenCommand
+        {
+            get
+            {
+                if (this.openCommand == null)
+                    this.openCommand = new RelayCommand(() => this.OpenFile(), () => this.CanOpenFile());
+                return this.openCommand;
+            }
+        }
+
+        public ICommand PlayCommand
+        {
+            get
+            {
+                if (this.playCommand == null)
+                    this.playCommand = new RelayCommand(() => this.PlayFile(), () => this.CanPlayFile());
+                return this.playCommand;
+            }
+        }
+
+        public ICommand PauseCommand
+        {
+            get
+            {
+                if (this.pauseCommand == null)
+                    this.pauseCommand = new RelayCommand(() => this.PauseFile(), () => this.CanPauseFile());
+                return this.pauseCommand;
+            }
+        }
+
+        public ICommand StopCommand
+        {
+            get
+            {
+                if (this.stopCommand == null)
+                    this.stopCommand = new RelayCommand(() => this.StopFile(), () => this.CanStopFile());
+                return this.stopCommand;
+            }
+        }
+        #endregion
+
+        private bool CanOpenFile()
+        {
+            return true;
+        }
+
+        private void OpenFile()
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            openFile.Filter = "All files (*.*)|*.*";
+            openFile.FilterIndex = 2;
+            openFile.RestoreDirectory = true;
+            openFile.Title = "Open...";
+            openFile.ShowDialog();
+            if (openFile.FileNames.Length > 0)
+            {
+                foreach (string filename in openFile.FileNames)
+                {
+                    media.Source(filename);
+                    media.Play();
+                }
+            }
+        }
+
+        private bool CanPlayFile()
+        {
+            return true;
+        }
+
+        private void PlayFile()
+        {
+            media.Play();
+        }
+
+        private bool CanPauseFile()
+        {
+            return true;
+        }
+
+        private void PauseFile()
+        {
+            media.Pause();
+        }
+
+        private bool CanStopFile()
+        {
+            return true;
+        }
+
+        private void StopFile()
+        {
+            media.Stop();
+        }
+    }
+}
